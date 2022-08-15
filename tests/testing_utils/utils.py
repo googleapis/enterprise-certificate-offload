9a5812e4ca3a03c2d11cb01cf0ab21ba29c6e16e
clundin25 <108372512+clundin25@users.noreply.github.com>
2022-08-15 21:01:47 +0000

    feat: Sync code (#7)
    * Adding src and test offload code
    
    Co-authored-by: Sai Sunder Srinivasan <saisunder@google.com>

---
+++ tests/testing_utils/utils.py
@@ -0,0 +1,52 @@
+import json
+import os
+
+from sys import platform
+from tempfile import NamedTemporaryFile
+
+testing_utils_folder_path = os.path.dirname(os.path.abspath(__file__))
+signer_binaries_folder = os.path.join(testing_utils_folder_path, "signer_binaries")
+build_folder = os.path.join(testing_utils_folder_path, os.pardir, os.pardir, "build")
+
+def _generate_mac_enterprise_cert_json(issuer):
+    if issuer is None:
+        issuer = "Google Endpoint Verification"
+
+    cert_info = { "issuer" : issuer }
+
+    libs = {
+        "signer_binary": os.path.join(signer_binaries_folder, "mac64", "signer"),
+        "signer_library": os.path.join(signer_binaries_folder, "mac64", "signer.dylib"),
+        "offload_library": os.path.join(build_folder, "offload_mac64.dylib")
+    }
+
+    enterprise_cert_dict = {
+        "cert_info": cert_info,
+        "libs": libs,
+        "version": "1"
+    }
+
+    return json.dumps(enterprise_cert_dict)
+
+def _generate_win_enterprise_cert_json(issuer):
+    return ""
+
+def _generate_linux_enterprise_cert_json(issuer):
+    return ""
+
+def _write_enterprise_cert_json(contents):
+    tmp_file = NamedTemporaryFile(delete=True)
+    with open(tmp_file.name, "w") as f:
+        f.write(contents)
+
+    return tmp_file
+
+def generate_enterprise_cert_file(issuer = None):
+    if platform == "win32":
+        enterprise_cert_json = _generate_win_enterprise_cert_json(issuer)
+    elif platform == "darwin":
+        enterprise_cert_json = _generate_mac_enterprise_cert_json(issuer)
+    else:
+        enterprise_cert_json = _generate_linux_enterprise_cert_json(issuer)
+    return _write_enterprise_cert_json(enterprise_cert_json)
+    
