d3b8f6b99b04f8151d1a1022a1e3aa8b5609cce2
Carl Lundin <clundin@google.com>
2022-08-15 16:28:51 -0700

    chore: Add License header to files containing code.

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -1,3 +1,7 @@
+# Copyright 2022 Google LLC.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 #!/bin/bash
 
 g++ -fPIC -c ./src/offload.cpp -m64 -std=c++11
